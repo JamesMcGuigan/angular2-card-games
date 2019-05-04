7260afa37f1714499b0e29c9ed3d8a35bbe94f9b
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-05-04 23:32:48 +0100

    Implement http-server and single-page-app HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { PageOneCardDrawComponent } from './page-one-card-draw/page-one-card-draw.component';
-import { HashLocationComponent } from './hash-location/hash-location.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {PageOneCardDrawComponent} from './page-one-card-draw/page-one-card-draw.component';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     PageOneCardDrawComponent,
-    HashLocationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }  // OR: app-routing.module.ts: RouterModule.forRoot(routes, { useHash: true }),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-menu',
+  selector:    'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.less']
+  styleUrls:  ['./menu.component.less']
 })
 export class MenuComponent implements OnInit {
 
+  // TODO: figure out how to extract route information from AppRoutingModule
   links = [
+    { url: '', title: 'Home' },
     { url: 'one-card-draw', title: 'One Card Draw' }
   ];
 
