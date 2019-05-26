259e26af5335691c7730fe7be65f57570b497b02
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-05-26 21:58:26 +0100

    Zeit Now Config

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-pwd
-ls -la
 npm install
 npm install --only=dev
-ls -la
-ls -la ./node_modules/.bin/
 npm run now-build
-./node_modules/.bin/ng build --prod -c production --aot --output-path ./dist/
